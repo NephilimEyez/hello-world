d01e3cd5fca4af00133bad1b96a42860e5ae618c
NephilimEyez <34822464+NephilimEyez@users.noreply.github.com>
2023-04-20 18:16:45 -0400

    Can't figure out why dave is on new line

--- app.js
+++ app.js
@@ -5,10 +5,19 @@ function getName() {
     return userName;
 }
 
-function greetUser(theirName) {
-    if (theirName === undefined) {
-        document.write("Hello, Dave.");        
-    } else {
+function greetUser() {
+    if (theirName == null || undefined) {
+        let tempName = prompt ("Are you sure you don't want a more personalized experience?");
+        if (tempName == null || undefined) {
+            document.write("Hello, <p id='noInput'>Dave.</p>"); 
+        } else {
+            console.log("not working")
+            document.write("Hello, " + tempName + ".");
+        }
+             
+    } else { 
         document.write("Hello, " + theirName + ".");
     }
 }
+
+let theirName = getName();
