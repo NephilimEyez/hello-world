f07f3bf492f86dda6fea154a6fbf0a306c8f8ce7
NephilimEyez <34822464+NephilimEyez@users.noreply.github.com>
2023-04-21 09:10:15 -0400

    Finished lab 8 I hope

--- app.js
+++ app.js
@@ -11,12 +11,25 @@ function greetUser() {
         if (tempName == null || undefined) {
             document.write('Hello, <p id="noInput">Dave.</p>'); 
         } else {
-            console.log("not working")
-            document.write("Hello, " + tempName + ".");
+            console.log('not working')
+            document.write('Hello, ' + tempName + '.');
         }
              
     } else { 
         document.write("Hello, " + theirName + ".");
+    }
+}
+
+function star() {
+    let raiting = prompt ("How many stars betwee 1-5 would you rate my site.")
+    if (raiting > 5) {
+        raiting = 5;
+    } else if (raiting <= 0){ 
+        document.write('<img src="images/sadface.jpg" alt="sad face">');
+    } else {
+    }
+    for (let i = raiting; i > 0; i--) {
+        document.write('<img src="https://cdn.cnn.com/cnnnext/dam/assets/220106111824-red-supergiant-star-supernova-small-11.jpg" alt="Nasa Image">')
     }
 }
 
